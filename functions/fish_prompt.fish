b1abe4a62ace7b1c125bb55df9a2729e47724974
seigenji <admin@h46.170.236.10.32437.vlan.kuins.net>
2017-04-25 22:09:06 +0900

    ステータスによってアドレスパスの色を変えた

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,9 +1,8 @@
 function fish_prompt
-	test $SSH_TTY
-    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
-    test $USER = 'root'
-    and echo (set_color red)"#"
-
-    # Main
-    echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    if [ $status -eq 0 ]
+        echo -n (set_color cyan)(prompt_pwd)
+    else
+        echo -n (set_color red --bold)(prompt_pwd)
+    end
+    echo -n '' (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end
