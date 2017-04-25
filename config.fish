c05c242a5e75c396d6cddaab0f585f861bcdcce8
seigenji <admin@h46.170.236.10.32437.vlan.kuins.net>
2017-04-25 22:43:37 +0900

    Add “Press enter to ls” but print nothing

--- config.fish
+++ config.fish
@@ -21,3 +21,27 @@ end
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/opt/openssl/bin $PATH
 set -x PATH /usr/local/opt/opencv3/bin $PATH
+
+# Press enter to ls
+function do_enter
+  set -l query (commandline)
+
+  if test -n $query
+    echo
+    eval $query
+    commandline ''
+  else
+    echo
+    ls
+    if test (git rev-parse --is-inside-work-tree 2> /dev/null)
+      echo
+      echo -e "\e[0;33m--- git status ---\e[0m"
+      git status -sb
+    end
+  end
+  commandline -f repaint
+end
+
+function fish_user_key_bindings
+  bind \cj do_enter
+end
