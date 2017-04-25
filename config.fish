bd32a55edcbcfb83f9ac359476e1e7b01dfc7f3d
seigenji <admin@h46.170.236.10.32437.vlan.kuins.net>
2017-04-26 01:52:22 +0900

    Remove if-clause of Login definition

--- config.fish
+++ config.fish
@@ -1,33 +1,29 @@
-##
-# LOGIN Definition
-##
-if status --is-login
-  # For Japanese
-  set -x LANG ja_JP.UTF-8
-  set -x LESSCHARSET utf-8
-  set -x JLESSCHARSET japanese-utf-8
+# For Japanese
+set -x LANG ja_JP.UTF-8
+set -x LESSCHARSET utf-8
+set -x JLESSCHARSET japanese-utf-8
 
-  # For Python 2.7
-  set -x HGENCODING UTF-8
+# For Python 2.7
+set -x HGENCODING UTF-8
 
-  # cdした後に自動でls
-  function cd
-    builtin cd $argv
+# cdした後に自動でls
+function cd
+  builtin cd $argv
+  ls
+end
+
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/opt/openssl/bin $PATH
+set -x PATH /usr/local/opt/opencv3/bin $PATH
+set -g fish_user_paths "/usr/local/opt/opencv3/bin" $fish_user_paths
+
+# Press enter to ls
+function done_enter --on-event fish_postexec
+  if test -z "$argv"
     ls
   end
-
-  set -x PATH /usr/local/bin $PATH
-  set -x PATH /usr/local/opt/openssl/bin $PATH
-  set -x PATH /usr/local/opt/opencv3/bin $PATH
-  set -g fish_user_paths "/usr/local/opt/opencv3/bin" $fish_user_paths
-
-  # Press enter to ls
-  function done_enter --on-event fish_postexec
-    if test -z "$argv"
-      ls
-    end
-  end
 end
+
 
 # sublimetext3を現在のディレクトリをプロジェクトとして開く
 # commandは sb . とかで今のディレクトリをプロジェクトとして開く
