8cd352585e08e204a1d59c28641b438c4e835a68
seigenji <admin@h46.170.236.10.32437.vlan.kuins.net>
2017-04-25 22:53:34 +0900

    Remove GitHub status setting

--- config.fish
+++ config.fish
@@ -2,20 +2,19 @@
 # LOGIN Definition
 ##
 if status --is-login
+  # For Japanese
+  set -x LANG ja_JP.UTF-8
+  set -x LESSCHARSET utf-8
+  set -x JLESSCHARSET japanese-utf-8
 
- # For Japanese
- set -x LANG ja_JP.UTF-8
- set -x LESSCHARSET utf-8
- set -x JLESSCHARSET japanese-utf-8
-
- # For Python 2.7
- set -x HGENCODING UTF-8
+  # For Python 2.7
+  set -x HGENCODING UTF-8
 end
 
 # cdした後に自動でls
 function cd
   builtin cd $argv
-    ls
+  ls
 end
 
 set -x PATH /usr/local/bin $PATH
@@ -23,25 +22,8 @@ set -x PATH /usr/local/opt/openssl/bin $PATH
 set -x PATH /usr/local/opt/opencv3/bin $PATH
 
 # Press enter to ls
-function do_enter
-  set -l query (commandline)
-
-  if test -n $query
-    echo
-    eval $query
-    commandline ''
-  else
-    echo
+function done_enter --on-event fish_postexec
+  if test -z "$argv"
     ls
-    if test (git rev-parse --is-inside-work-tree 2> /dev/null)
-      echo
-      echo -e "\e[0;33m--- git status ---\e[0m"
-      git status -sb
-    end
   end
-  commandline -f repaint
-end
-
-function fish_user_key_bindings
-  bind \cj do_enter
 end
